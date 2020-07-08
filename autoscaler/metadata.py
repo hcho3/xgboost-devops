5c007b6dcac6d37dc3819967a53efc8e76e93d99
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-07-08 23:45:35 +0000

    Add missing worker labels

--- autoscaler/metadata.py
+++ autoscaler/metadata.py
@@ -1,5 +1,5 @@
 from typing import Set
 
 def recognized_jenkins_worker_labels() -> Set[str]:
-    return {'linux', 'win64', 'build', 'cuda9', 'cuda10', 'cuda10_0',
+    return {'linux', 'win64', 'build', 'cuda9', 'cuda10', 'cuda10_0', 'cuda10_1', 'cuda10_2',
             'cpu', 'cpu_build', 'gpu', 'mgpu', 'master'}
