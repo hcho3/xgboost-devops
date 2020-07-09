718e9570b950535c2769d67c7de98c87f0fb47f9
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-07-09 07:18:09 +0000

    Catch exceptions in handler; don't return anything

--- autoscaler/lambda_function.py
+++ autoscaler/lambda_function.py
@@ -8,18 +8,19 @@ from label_extractor import label_from_queued_job, managed_worker_label
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-def lambda_handler(event: Any, context: Any) -> Dict[str, Union[int, str]]:
-    handle = jenkins_handle()
-    queue_items = handle.get_queue()._data['items']
-    workers = handle.get_nodes()._data['computer']
-    job_labels = [label_from_queued_job(workers=workers, queued_job=x) for x in queue_items]
-    worker_labels = [managed_worker_label(x) for x in workers]
-    logger.info('Queued jobs\' label: %s', job_labels)
-    logger.info('Jenkins workers: %s', worker_labels)
-    return {
-        'statusCode': 200,
-        'body': {
-            'Queued jobs label': [(list(x) if x else None) for x in job_labels],
-            'Jenkins workers label': [(list(x) if x else None) for x in worker_labels]
-        }
-    }
+def lambda_handler(event: Any, context: Any):
+    """Hanlder to be called by AWS Lambda"""
+    try:
+        handle = jenkins_handle()
+        queue_items = handle.get_queue()._data['items']
+        workers = handle.get_nodes()._data['computer']
+        job_labels = [label_from_queued_job(workers=workers, queued_job=x) for x in queue_items]
+        worker_labels = [managed_worker_label(x) for x in workers]
+        logger.info('Queued jobs\' label: %s', job_labels)
+        logger.info('Jenkins workers: %s', worker_labels)
+    except Exception as e:
+        # This try-catch is important because we have to catch all exceptions. Otherwise, the
+        # exceptions bubble up to lambda and the service retries executing multiple times. We only
+        # want exactly one execution per request.
+        logging.exception('Unexpected exception: %s', e)
+        logging.fatal('Unexpected exception: %s', e)
--- autoscaler/local_runner.py
+++ autoscaler/local_runner.py
@@ -2,4 +2,4 @@ import lambda_function
 import logging
 
 logging.basicConfig(level=logging.INFO)
-print(lambda_function.lambda_handler(None, None))
+lambda_function.lambda_handler(None, None)
