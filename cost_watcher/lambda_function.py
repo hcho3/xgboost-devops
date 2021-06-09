ecdacad87b5a0bd65bf3121a74b08afe9b909fe4
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2021-06-09 16:35:24 -0700

    Add g4dn.8xlarge to recognized EC2 instance type

--- cost_watcher/lambda_function.py
+++ cost_watcher/lambda_function.py
@@ -14,7 +14,7 @@ from botocore.config import Config
 recognized_os_types = ['Linux', 'Windows']
 recognized_instance_types = [
     'c5a.4xlarge', 'c5.4xlarge', 'g4dn.xlarge', 'g4dn.12xlarge', 'p2.xlarge', 'c5.large',
-    't3a.large', 't3a.micro', 'g4dn.2xlarge', 'c6g.4xlarge'
+    't3a.large', 't3a.micro', 'g4dn.2xlarge', 'c6g.4xlarge', 'g4dn.8xlarge'
 ]
 no_launch_policy_arn = 'arn:aws:iam::492475357299:policy/EC2AccessNoRunInstances'
 
